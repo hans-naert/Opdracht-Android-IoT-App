b5d7fe032f4223cfe175b0a6e9451b0d28c388eb
Hans Naert <hans.naert@vives.be>
2025-03-02 15:02:34 +0100

    Handle click on image

--- app/src/main/java/be/viveselic/androidiotapp/MainActivity.kt
+++ app/src/main/java/be/viveselic/androidiotapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package be.viveselic.androidiotapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setOnClickListener {
+            if (imageView.tag == "lamp") {
+                imageView.tag = "lampoff"
+                imageView.setImageResource(R.drawable.lampoff)
+            } else {
+                imageView.tag = "lamp"
+                imageView.setImageResource(R.drawable.lamp)
+            }
         }
     }
 }
